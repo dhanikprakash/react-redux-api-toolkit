66e6ba7449cc24a96084c1330aa94a16bc75748d
Admin <dhanikprakash@gmail.com>
2022-06-04 18:35:40 +0100

    update fetch to arrow function

--- src/api/fetch-data.ts
+++ src/api/fetch-data.ts
@@ -1,12 +1,15 @@
+import { MusicBO } from "./../BO/music";
 import axios from "axios";
 
-export async function fetchData(searchQuery: string) {
+export const fetchData = async (
+  searchQuery: string
+): Promise<MusicBO[] | undefined> => {
   try {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
-    return response.data;
+    return response.data as MusicBO[];
   } catch (error) {
     console.error(error);
   }
-}
+};
